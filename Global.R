db875dc8e4e09623b56c87ef2c353b989ae61e7c
alex-conanec <alexandreconanec@gmail.com>
2019-03-30 17:11:12 +0100

    Set up a backup functionality for the scores dataframe

--- Global.R
+++ Global.R
@@ -1,18 +1,27 @@
 require(shiny)
 require(shinyjs)
 require(shinyBS)
+require(rdrop2)
 
-source('R/fonctions.R')
+token <- readRDS("droptoken.rds")
+drop_acc(dtoken = token)
+output_dir <- "Tarot"
+
+eval(parse('R/db.R', encoding="UTF-8"))
 eval(parse('R/form.R', encoding="UTF-8"))
 eval(parse('R/scores_display.R', encoding="UTF-8"))
 eval(parse('R/scores_calculation.R', encoding="UTF-8"))
 eval(parse('R/stats.R', encoding="UTF-8"))
 
-scores <- read.csv("www/scores.csv", sep = ';', 
-                   stringsAsFactors=FALSE, dec=',')
+backup(dtoken = token)
+
+scores <- drop_read_rds("www/scores.RDS", output_dir = output_dir,
+                        dtoken = token)
+                          
 
 players_names_col <- seq(from=which(colnames(scores)=='Annonces') + 1, 
                          to= ncol(scores))
 
 players <- colnames(scores)[players_names_col]
-
+# scores <- read.csv('www/scores.csv', sep=';', dec=',')
+# saveRDS(scores, "www/scores.RDS")
